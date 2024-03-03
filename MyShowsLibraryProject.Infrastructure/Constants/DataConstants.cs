e014841d70338b91c0115187c8a6be7ce725fcbb
georg <georgi.donev984@gmail.com>
2024-03-03 21:18:18 +0200

    Adding DataConstants to the infrastructure part of the project

---
+++ MyShowsLibraryProject.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,28 @@
+﻿namespace MyShowsLibraryProject.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        //Universal constants
+        public const int TitleMaxLength = 255;
+        public const int UrlsMaxLength = 255;
+        public const int YearMaxLength = 4;
+        public const int SummaryMaxLength = 255;
+        //Movie
+        public const int MovieLenguageMaxLength = 30;
+        //Serie constants
+        public const int SerieLenguageMaxLength = 30;
+        //Season constants
+        public const int SeasonEpisodeMaxLength = 5;
+        //Crew constants
+        public const int CrewNameMaxLength = 50;
+        public const int CrewPseudonymMaxLength = 50;
+        public const int CrewNationalityMaxLength = 30;
+        public const int CrewBiographyMaxLength = 8000;
+        //Review constants 
+        public const int ReviewContentMaxLength = 8000;
+        //Genre constants
+        public const int GenreNameMaxLength = 30;
+        //Role constants
+        public const int RoleNameMaxLength = 30;
+    }
+}

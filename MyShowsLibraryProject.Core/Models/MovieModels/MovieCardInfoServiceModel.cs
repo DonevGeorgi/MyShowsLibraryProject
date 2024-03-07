c4a73f660edd96a60005972ac3aeef5c11703dec
georg <georgi.donev984@gmail.com>
2024-03-07 22:19:44 +0200

    Adding models for the services.

---
+++ MyShowsLibraryProject.Core/Models/MovieModels/MovieCardInfoServiceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MyShowsLibraryProject.Core.Models.MovieModels
+{
+    public class MovieCardInfoServiceModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string YearOfRelease { get; set; } = string.Empty;
+        public string PosterUrl { get; set; } = string.Empty;
+        public string Rating { get; set; } = string.Empty;
+    }
+}
---
+++ MyShowsLibraryProject.Core/Models/SerieModels/SeriesCardInfoServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyShowsLibraryProject.Core.Models.SerieModels
+{
+    public class SeriesCardInfoServiceModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string PosterUrl { get; set; } = string.Empty;
+        public string Rating { get; set; } = string.Empty;
+    }
+}

39a4d0eedb3db793c6fe062abebb2a8dc8d263d0
georg <georgi.donev984@gmail.com>
2024-03-17 16:40:42 +0200

    Changing Movie and Serie Views to display movie crew.

--- MyShowsLibraryProject.Infrastructure/Data/Models/Crew.cs
+++ MyShowsLibraryProject.Infrastructure/Data/Models/Crew.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MyShowsLibraryProject.Infrastructure.Constants;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyShowsLibraryProject.Infrastructure.Data.Models
 {

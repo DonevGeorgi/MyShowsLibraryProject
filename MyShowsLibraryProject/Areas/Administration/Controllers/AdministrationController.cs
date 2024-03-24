c7f4563b5536ced34e3fa2992736320e87c650ea
georg <georgi.donev984@gmail.com>
2024-03-24 14:26:42 +0200

    Adding admin area and controllers for home and administration base, additionaly adding view logic fot the layout.

---
+++ MyShowsLibraryProject/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyShowsLibraryProject.Areas.Administration.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    [Area("Administration")]
+    public class AdministrationController : Controller
+    {
+    }
+}

410a4086adce6d69d9ee550bdad1c03e22a8a461
georg <georgi.donev984@gmail.com>
2024-03-28 22:31:09 +0200

    Adding controller and view in the administrator area for the episodes.

---
+++ MyShowsLibraryProject/Areas/Administration/Controllers/SeasonController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShowsLibraryProject.Core.Models.SeasonModels;
+using MyShowsLibraryProject.Core.Services.Contacts;
+
+namespace MyShowsLibraryProject.Areas.Administration.Controllers
+{
+    public class SeasonController : AdministrationController
+    {
+        private readonly ISeasonService seasonService;
+
+        public SeasonController(ISeasonService _seasonService)
+        {
+            seasonService = _seasonService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int seriesId)
+        {
+            var model = await seasonService.GetAllSeasonForSeries(seriesId);
+
+               TempData["identitfier"] = seriesId;
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var entity = new SeasonFormModel();
+            
+            return View(entity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(SeasonFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var entity = new SeasonFormModel();
+
+                return View(entity);
+            }
+
+            var seriesId = (int)TempData["identitfier"];
+
+            var seasonId = await seasonService.CreateAsync(model,seriesId);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}

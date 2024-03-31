0bbf3ea5247f7a8dbd9d4e5c404b6c68e85ffc5e
georg <georgi.donev984@gmail.com>
2024-03-31 20:37:37 +0300

    Adding serie genre service to take part of genre service work.

---
+++ MyShowsLibraryProject.Core/Services/Contacts/ISerieGenreService.cs
@@ -0,0 +1,11 @@
+﻿using MyShowsLibraryProject.Core.Models.GenreModels;
+
+namespace MyShowsLibraryProject.Core.Services.Contacts
+{
+    public interface ISerieGenreService
+    {
+        Task<IEnumerable<GenreInfoSeviceModel>> TakeAllGenres();
+        Task AddGenreToSerieAsync(int serieId, int genreId);
+        Task RemoveGenreFromSerieAsync(int serieId, int genreId);
+    }
+}
---
+++ MyShowsLibraryProject.Core/Services/SerieGenreService.cs
@@ -0,0 +1,81 @@
+﻿using MyShowsLibraryProject.Core.Models.GenreModels;
+using MyShowsLibraryProject.Core.Services.Contacts;
+using MyShowsLibraryProject.Infrastructure.Data.Common;
+using MyShowsLibraryProject.Infrastructure.Data.Models;
+
+namespace MyShowsLibraryProject.Core.Services
+{
+    public class SerieGenreService : ISerieGenreService
+    {
+        private readonly IRepository repository;
+        private readonly ISerieService serieService;
+        private readonly IGenreService genreService;    
+
+        public SerieGenreService(IRepository _repository,
+            ISerieService _serieService,
+            IGenreService _genreService)
+        {
+            repository = _repository;
+            serieService = _serieService;
+            genreService = _genreService;
+        }
+
+        public async Task<IEnumerable<GenreInfoSeviceModel>> TakeAllGenres()
+        {
+            var genres = await genreService.GetAllReadonlyAsync();
+
+            return genres;
+        }
+        public async Task AddGenreToSerieAsync(int serieId, int genreId)
+        {
+            var serie = await serieService.GetSerieDetailsByIdAsync(serieId);
+
+            if (serie == null)
+            {
+                throw new NullReferenceException("Serie you chose dont exist!");
+            }
+
+            if (serie.Genres.Any(g => g.GenreId == genreId))
+            {
+                throw new ArgumentException("Serie contains genre already!");
+            }
+
+            var newSerieGenre = new SerieGenre()
+            {
+                SerieId = serie.SerieId,
+                GenreId = genreId
+            };
+
+            await repository.AddAsync(newSerieGenre);
+            await repository.SaveChangesAsync();
+        }
+        public async Task RemoveGenreFromSerieAsync(int serieId, int genreId)
+        {
+            var serie = await serieService.GetSerieDetailsByIdAsync(serieId);
+
+            if (serie == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            if (!serie.Genres.Any())
+            {
+                throw new ArgumentException("Serie dont have genres!");
+            }
+
+            if (!serie.Genres.Any(g => g.GenreId == genreId))
+            {
+                throw new ArgumentException("Serie dont have chosen genre!");
+            }
+
+            var modelToRemove = new SerieGenre()
+            {
+                SerieId = serie.SerieId,
+                GenreId = genreId
+            };
+
+            repository.Remove<SerieGenre>(modelToRemove);
+            await repository.SaveChangesAsync();
+        }
+    }
+}

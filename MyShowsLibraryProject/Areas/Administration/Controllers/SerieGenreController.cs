80fc64e379e919a5a1c43b2796d2bb470cb4f0c2
georg <georgi.donev984@gmail.com>
2024-03-31 20:41:49 +0300

    Adding serie genre controller and views to take part of genre controller work.

---
+++ MyShowsLibraryProject/Areas/Administration/Controllers/SerieGenreController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShowsLibraryProject.Core.Models.GenreModels;
+using MyShowsLibraryProject.Core.Services.Contacts;
+
+namespace MyShowsLibraryProject.Areas.Administration.Controllers
+{
+    public class SerieGenreController : AdministrationController
+    {
+        private readonly ISerieGenreService serieGenreService;
+
+        public SerieGenreController(ISerieGenreService _serieGenreService)
+        {
+            serieGenreService = _serieGenreService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddGenreToSerie(int serieId)
+        {
+            var model = new GenreChoseFromModel()
+            {
+                GenresName = await serieGenreService.TakeAllGenres()
+            };
+
+            if (model.GenresName.Any() == false)
+            {
+                return NotFound();
+            }
+
+            TempData["serieIdentifier"] = serieId;
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddGenreToSerie(GenreChoseFromModel genre)
+        {
+            var serieId = Convert.ToInt32(TempData["serieIdentifier"]);
+
+            await serieGenreService.AddGenreToSerieAsync(serieId, genre.GenreId);
+
+            return RedirectToAction("Index", "Serie");
+        }
+        [HttpGet]
+        public async Task<IActionResult> RemoveGenreFromSerie(int serieId)
+        {
+            var model = new GenreChoseFromModel()
+            {
+                GenresName = await serieGenreService.TakeAllGenres()
+            };
+
+            if (model.GenresName.Any() == false)
+            {
+                return NotFound();
+            }
+
+            TempData["serieIdentifier"] = serieId;
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> RemoveGenreFromSerie(GenreChoseFromModel genre)
+        {
+            var serieId = Convert.ToInt32(TempData["serieIdentifier"]);
+
+            await serieGenreService.RemoveGenreFromSerieAsync(serieId, genre.GenreId);
+
+            return RedirectToAction("Index", "Serie");
+        }
+    }
+}

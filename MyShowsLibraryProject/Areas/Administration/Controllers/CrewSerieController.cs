4a2b548c4e8ca6f84b8dd224e33f92f527cb9971
georg <georgi.donev984@gmail.com>
2024-04-03 21:32:43 +0300

    Adding serie crew controller and views in the administrator area.

---
+++ MyShowsLibraryProject/Areas/Administration/Controllers/CrewSerieController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShowsLibraryProject.Core.Models.CrewModels;
+using MyShowsLibraryProject.Core.Services.Contacts;
+
+namespace MyShowsLibraryProject.Areas.Administration.Controllers
+{
+    public class CrewSerieController : AdministrationController
+    {
+        private readonly ICrewSerieService crewSerieService;
+
+        public CrewSerieController(ICrewSerieService _crewSerieService)
+        {
+            crewSerieService = _crewSerieService;
+        }
+
+        [HttpGet]
+        public IActionResult AddCrewToSerie(int serieId)
+        {
+            var model = new CrewNameFromModel() 
+            { 
+                
+            };
+
+            TempData["serieIdentifier"] = serieId;
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCrewToSerie(CrewNameFromModel newCrew)
+        {
+            if (!ModelState.IsValid)
+            {
+                var model = new CrewNameFromModel()
+                {
+
+                };
+
+                return View();
+            }
+
+            var serieId = Convert.ToInt32(TempData["serieIdentifier"]);
+
+            await crewSerieService.AddCrewToSerie(serieId, newCrew.CrewName);
+
+            return RedirectToAction("Index", "Serie");
+        }
+        [HttpGet]
+        public IActionResult RemoveCrewFromSerie(int serieId)
+        {
+            var model = new CrewNameFromModel()
+            {
+
+            };
+
+            TempData["serieIdentifier"] = serieId;
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> RemoveCrewFromSerie(CrewNameFromModel newCrew)
+        {
+            if (!ModelState.IsValid)
+            {
+                var model = new CrewNameFromModel()
+                {
+
+                };
+
+                return View();
+            }
+
+            var serieId = Convert.ToInt32(TempData["serieIdentifier"]);
+
+            await crewSerieService.RemoveCrewFromSerie(serieId, newCrew.CrewName);
+
+            return RedirectToAction("Index", "Serie");
+        }
+    }
+}

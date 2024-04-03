8ca672e1d5ab20f6ec069666d58a04e7dd489273
georg <georgi.donev984@gmail.com>
2024-04-03 21:30:32 +0300

    Adding serie crew service.

---
+++ MyShowsLibraryProject.Core/Services/Contacts/ICrewSerieService.cs
@@ -0,0 +1,9 @@
+﻿namespace MyShowsLibraryProject.Core.Services.Contacts
+{
+    public interface ICrewSerieService
+    {
+        Task<int> GetCrewName(string crewName);
+        Task AddCrewToSerie(int serieId, string crewName);
+        Task RemoveCrewFromSerie(int serieId, string crewName);
+    }
+}
---
+++ MyShowsLibraryProject.Core/Services/CrewSerieService.cs
@@ -0,0 +1,87 @@
+﻿using MyShowsLibraryProject.Core.Services.Contacts;
+using MyShowsLibraryProject.Infrastructure.Data.Common;
+using MyShowsLibraryProject.Infrastructure.Data.Models;
+
+namespace MyShowsLibraryProject.Core.Services
+{
+    public class CrewSerieService : ICrewSerieService
+    {
+        private readonly IRepository repository;
+        private readonly ICrewService crewService;
+        private readonly ISerieService serieService;
+
+        public CrewSerieService(IRepository _repository,
+            ICrewService _crewService,
+            ISerieService _serieService)
+        {
+            repository = _repository;
+            crewService = _crewService;
+            serieService = _serieService;
+        }
+
+        public async Task<int> GetCrewName(string crewName)
+        {
+            var crew = await crewService.GetCrewName(crewName);
+
+            if (crew == 0)
+            {
+                throw new ArgumentException("Crew with this name does not exists!");
+            }
+
+            return crew;
+        }
+        public async Task AddCrewToSerie(int serieId, string crewName)
+        {
+            var serie = await serieService.GetSerieDetailsByIdAsync(serieId);
+            var crewId = await GetCrewName(crewName);
+
+            if (serie == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            if (serie.Crews.Any(g => g.Name == crewName))
+            {
+                throw new ArgumentException("Serie contains crew already!");
+            }
+
+            var newSerieCrew = new SerieCrew()
+            {
+                SerieId = serie.SerieId,
+                CrewId = crewId
+            };
+
+            await repository.AddAsync(newSerieCrew);
+            await repository.SaveChangesAsync();
+        }
+        public async Task RemoveCrewFromSerie(int serieId, string crewName)
+        {
+            var serie = await serieService.GetSerieDetailsByIdAsync(serieId);
+            var crewId = await GetCrewName(crewName);
+
+            if (serie == null)
+            {
+                throw new NullReferenceException("Serie you chose dont exist!");
+            }
+
+            if (!serie.Crews.Any())
+            {
+                throw new ArgumentException("Serie dont have any crew!");
+            }
+
+            if (!serie.Crews.Any(c => c.CrewId == crewId))
+            {
+                throw new ArgumentException("Movie dont have this crew!");
+            }
+
+            var modelToRemove = new SerieCrew()
+            {
+                SerieId = serie.SerieId,
+                CrewId = crewId
+            };
+
+            repository.Remove<SerieCrew>(modelToRemove);
+            await repository.SaveChangesAsync();
+        }
+    }
+}

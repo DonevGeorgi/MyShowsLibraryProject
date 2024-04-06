f61ed24839c06540f7f5afb9e17f17f667b36180
georg <georgi.donev984@gmail.com>
2024-04-06 15:58:21 +0300

    Adding query models to service models and changing method in movie service.

---
+++ MyShowsLibraryProject.Core/Models/MovieModels/MoviesQueryModel.cs
@@ -0,0 +1,16 @@
+﻿using MyShowsLibraryProject.Core.Enumeration;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyShowsLibraryProject.Core.Models.MovieModels
+{
+    public class MoviesQueryModel
+    {
+        public int MoviePerPage = 12;
+        [Display(Name = "Search by text")]
+        public string SearchTerm { get; set; } = string.Empty;
+        public MovieSorting Sorting { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        public int TotalMoviesCount { get; set; }
+        public IEnumerable<MoviesCardInfoServiceModel> Movies {  get; set; } = new List<MoviesCardInfoServiceModel>();
+    }
+}
---
+++ MyShowsLibraryProject.Core/Models/MovieModels/MoviesQueryServiceModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyShowsLibraryProject.Core.Models.MovieModels
+{
+    public class MoviesQueryServiceModel
+    {
+        public int TotalMovieCount {  get; set; }
+        public IEnumerable<MoviesCardInfoServiceModel> Movies {  get; set; } = new List<MoviesCardInfoServiceModel>();
+    }
+}

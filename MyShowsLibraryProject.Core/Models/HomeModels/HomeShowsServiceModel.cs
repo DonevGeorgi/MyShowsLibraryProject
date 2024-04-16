ce001f5d3101633a151f14f1e288690be22629fe
georg <georgi.donev984@gmail.com>
2024-04-16 21:37:20 +0300

    Adding models for home service.

---
+++ MyShowsLibraryProject.Core/Models/HomeModels/HomeShowsServiceModel.cs
@@ -0,0 +1,13 @@
+﻿using MyShowsLibraryProject.Core.Models.MovieModels;
+using MyShowsLibraryProject.Core.Models.SerieModels;
+
+namespace MyShowsLibraryProject.Core.Models.HomeModels
+{
+    public class HomeShowsServiceModel
+    {
+        public IEnumerable<MoviesCardInfoServiceModel> Movies { get; set; } = new List<MoviesCardInfoServiceModel>();
+        public IEnumerable<SeriesCardInfoServiceModel> Series { get; set; } = new List<SeriesCardInfoServiceModel>();
+        public MoviesCardInfoServiceModel HighestRatedMovies { get; set; } = null!;
+        public SeriesCardInfoServiceModel HighestRatedSeries { get; set; } = null!;
+    }
+}

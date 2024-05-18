09e87a5fa0d96f09d51cc15b0f4b219201e68b8f
georg <georgi.donev984@gmail.com>
2024-05-18 17:31:28 +0300

    Adding TakeAllCrews method to CrewSeries service.

--- MyShowsLibraryProject.Core/Services/Contacts/ICrewSerieService.cs
+++ MyShowsLibraryProject.Core/Services/Contacts/ICrewSerieService.cs
@@ -1,7 +1,10 @@
-﻿namespace MyShowsLibraryProject.Core.Services.Contacts
+﻿using MyShowsLibraryProject.Core.Models.CrewModels;
+
+namespace MyShowsLibraryProject.Core.Services.Contacts
 {
     public interface ICrewSerieService
     {
+        Task<IEnumerable<CrewInfoServiceModel>> TakeAllCrews(int serieId);
         Task AddCrewToSerie(int serieId, string crewName);
         Task RemoveCrewFromSerie(int serieId, string crewName);
     }

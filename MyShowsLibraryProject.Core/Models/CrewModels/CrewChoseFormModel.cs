5f3f0fee34ac5c586157c198fd02b9d75abd089d
georg <georgi.donev984@gmail.com>
2024-05-18 17:33:00 +0300

    Adding DTO model for crew chose menu.

---
+++ MyShowsLibraryProject.Core/Models/CrewModels/CrewChoseFormModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyShowsLibraryProject.Core.Models.CrewModels
+{
+    public class CrewChoseFormModel
+    {
+        public int CrewId { get; set; }
+        public IEnumerable<CrewInfoServiceModel> CrewNames { get; set; } = new List<CrewInfoServiceModel>();
+    }
+}

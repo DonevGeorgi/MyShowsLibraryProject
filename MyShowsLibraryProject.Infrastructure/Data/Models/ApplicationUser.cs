0b95cb74be0c71db34942769c66add1027cf3c56
georg <georgi.donev984@gmail.com>
2024-05-20 21:50:14 +0300

    Adding new application user model.

---
+++ MyShowsLibraryProject.Infrastructure/Data/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿ using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MyShowsLibraryProject.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyShowsLibraryProject.Infrastructure.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(DataConstants.FirstNameMaxLength)]
+        [PersonalData]
+        [Comment("User first name")]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(DataConstants.LastNameMaxLength)]
+        [PersonalData]
+        [Comment("User last name")]
+        public string LastName { get; set; } = string.Empty;
+        public IEnumerable<Post> Posts { get; set; } = new List<Post>();
+    }
+}

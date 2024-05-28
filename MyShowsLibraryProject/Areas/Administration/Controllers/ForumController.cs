829a90b6a18803fa1285681836cfe9e19cc00e39
georg <georgi.donev984@gmail.com>
2024-05-28 21:41:21 +0300

    Adding controller for the forum in the administrator area.

---
+++ MyShowsLibraryProject/Areas/Administration/Controllers/ForumController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShowsLibraryProject.Core.Models.ForumModels;
+using MyShowsLibraryProject.Core.Models.MovieModels;
+using MyShowsLibraryProject.Core.Services;
+using MyShowsLibraryProject.Core.Services.Contacts;
+
+namespace MyShowsLibraryProject.Areas.Administration.Controllers
+{
+    public class ForumController : AdministrationController
+    {
+        private readonly IForumService forumService;
+
+        public ForumController(IForumService _forumService)
+        {
+            forumService = _forumService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var topics = await forumService.GetAllTopics();
+
+            return View(topics);
+        }
+        [HttpGet]
+        public IActionResult AddTopic()
+        {
+            var result = new TopicFormModel();
+
+            return View(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddTopic(TopicFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var entity = new TopicFormModel();
+
+                return View(entity);
+            }
+
+            await forumService.CreateTopic(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}

45c5171317dc02aecd8106042ab6a387752cbcad
georg <georgi.donev984@gmail.com>
2024-05-28 21:39:54 +0300

    Adding get all and create topic methods to the forum service.

--- MyShowsLibraryProject.Core/Services/Contacts/IForumService.cs
+++ MyShowsLibraryProject.Core/Services/Contacts/IForumService.cs
@@ -5,6 +5,8 @@ namespace MyShowsLibraryProject.Core.Services.Contacts
 {
     public interface IForumService
     {
+        Task<List<TopicCardsInfoServiceModel>> GetAllTopics();
         Task<ForumQueryServiceModel> ShowAllTopics(string? searchTerm, BaseSorting sorting, int currPage, int topicsPerPage);
+        Task CreateTopic(TopicFormModel model);
     }
 }

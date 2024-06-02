9236f942e904319ee0b37d6d94e737731b5eb09a
georg <georgi.donev984@gmail.com>
2024-06-02 16:11:12 +0300

    Adding implementation in the forum controller for the add post.

--- MyShowsLibraryProject/Controllers/ForumController.cs
+++ MyShowsLibraryProject/Controllers/ForumController.cs
@@ -2,6 +2,7 @@
 using MyShowsLibraryProject.Core.Models.ForumModels;
 using MyShowsLibraryProject.Core.Models.MovieModels;
 using MyShowsLibraryProject.Core.Services.Contacts;
+using System.Security.Claims;
 
 namespace MyShowsLibraryProject.Controllers
 {
@@ -28,7 +29,44 @@ namespace MyShowsLibraryProject.Controllers
         {
             var posts = await forumService.GetAllPostsForTopic(topicId);
 
+            TempData["identitfier"] = topicId;
+
             return View(posts);
+        }
+        [HttpGet]
+        public IActionResult AddPostToTopic()
+        {
+            var result = new PostFormModel();
+
+            return View(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddPostToTopic(PostFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var result = new PostFormModel();
+
+                return View(result);
+            }
+
+            var userId = User.GetId();
+
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            var topicId = Convert.ToInt32(TempData["identitfier"]);
+
+            if (topicId == 0)
+            {
+                return BadRequest();
+            }
+
+            await forumService.CreatePostAsync(model, userId,topicId);
+
+            return RedirectToAction(nameof(AllTopicPosts), new { topicId });
         }
     }
 }
